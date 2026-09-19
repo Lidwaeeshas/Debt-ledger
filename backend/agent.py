ec064acc8edeeeca22955b77da03aefa19ef2210
Usman Sagir Usman <walidsagir8@gmail.com>
2026-09-19 15:42:41 +0100

    Update agent.py

--- backend/agent.py
+++ backend/agent.py
@@ -49,7 +49,7 @@ def json_resonse(msg):
     Analyze the provided text block and extract the debt ledger information according to the schema.
     """
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemini-3.5-flash-lite",
         contents=msg,
         config=types.GenerateContentConfig(
             system_instruction=system_rules,
