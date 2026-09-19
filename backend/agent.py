ba08deb35b0b4081232b2bd6a5d02b8d07720eed
Usman Sagir Usman <walidsagir8@gmail.com>
2026-09-19 14:09:06 +0100

    Update agent.py

--- backend/agent.py
+++ backend/agent.py
@@ -50,7 +50,7 @@ def json_resonse(msg):
     """
     response = client.models.generate_content(
         model="gemini-3.8-flash",
-        input=msg,
+        contents=msg,
         config=types.GenerateContentConfig(
             system_instruction=system_rules,
             response_mime_type="application/json",
